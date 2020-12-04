99e85b8655d13edf46e57b6fe91b5e69a3c1116f
weijinqian <1184188277@qq.com>
2020-12-04 22:49:19 +0800

    代码结构调整

--- src/feature_handle/view_utils.py
+++ src/feature_handle/view_utils.py
@@ -15,7 +15,20 @@ import warnings
 warnings.filterwarnings("ignore")
 """
 各种特征视觉分析api调用
+1. 单变量分析（统计量与变量的分布）
+（1）箱型图；（2）distplot
+2. 双变量分析
+【1】连续和连续
+    （1）绘制散点图（2）计算相关性
+【2】类别和类别
+    （1）双向表（2）堆叠柱状图
+【3】类别和连续
+    （1）小提琴图
 
+"""
+
+"""
+单变量
 """
 
 
@@ -35,6 +48,102 @@ def paint_box(data, columns):
         sns.boxplot(data=pd.DataFrame(data[columns[i]]), orient="v", width=0.5)
         plt.ylabel(columns[i], fontsize=12)
     plt.show()
+
+
+# 绘制直方图和QQ图 都是用来看数据分布是否是正太分布的
+def paint_dist(data, columns, cols=6):
+    rows = len(columns)
+    plt.figure(figsize=(60, 20), dpi=50)
+
+    i = 0
+    for col in columns:
+        print(data[col])
+        # if np.all(data[col]) == 0:
+        #     continue
+        i += 1
+        ax = plt.subplot(7, 6, i)
+        sns.distplot(data[col], fit=stats.norm)
+        i += 1
+        ax = plt.subplot(7, 6, i)
+        res = stats.probplot(data[col], plot=plt)
+
+    plt.tight_layout()
+    plt.show()
+
+
+"""
+双变量
+"""
+
+
+# 堆叠图
+def paint_count(data, column_name, target):
+    plt.figure(figsize=(8, 6))
+    plt.title(str(column_name) + ' VS Label' + str(target))
+    ax = sns.countplot(column_name, hue='label', data=data)
+
+
+def paint_reg(train_data, column: str, y_name='target'):
+    """
+    绘制线性回归关系
+    :param y_name:
+    :param train_data:
+    :param column: 训练数据所在的列名
+    :return:
+    """
+    fools = 2
+    frows = 1
+    plt.figure(figsize=(8, 4), dpi=50)
+    ax = plt.subplot(1, 2, 1)
+    sns.regplot(x=column, y=y_name, data=train_data, ax=ax, scatter_kws={'marker': '.', 's': 3, 'alpha': 0.3},
+                line_kws={'color': 'k'})
+    plt.xlabel(column)
+    plt.ylabel(y_name)
+
+    ax = plt.subplot(1, 2, 2)
+    sns.displot(train_data[column].dropna())
+    plt.xlabel(column)
+
+    plt.show()
+
+
+def paint_heatmap(data: DataFrame, drop_columns, k):
+    """
+    绘制热力图
+    :type k: 阈值k
+    :return:
+    """
+    pd.set_option("display.max_columns", 10)
+    pd.set_option('display.max_rows', 10)
+    if drop_columns is not None:
+        data_1 = data.drop(drop_columns, axis=1)
+    else:
+        data_1 = data
+
+    data_corr = data_1.corr()
+    # 这个是整体的热力图
+    # ax = plt.subplots(figsize=(20, 16))
+    # ax = sns.heatmap(data_corr, vmax=.8, square=True, annot=True)
+    # plt.show()
+
+    # 这个是top10的热力图
+    cols = data_corr.nlargest(k, 'target')['target'].index
+    cm = np.corrcoef(data_1[cols].values.T)
+    hm = plt.subplots(figsize=(10, 10))
+    hm = sns.heatmap(data_1[cols].corr(), annot=True, square=True)
+    plt.show()
+
+
+# 小提琴图，用于分析类别和连续型变量
+def paint_violin(data: DataFrame, column):
+    plt.figure(figsize=[16, 10])
+    sns.violinplot(x=data['label'], y=data[column])
+    plt.show()
+
+
+"""
+异常值分析
+"""
 
 
 #   基于模型预测来发现异常数据，看结果感觉还不错
@@ -89,25 +198,9 @@ def find_outliers(model, X, y, sigma=3):
     return outliers
 
 
-# 绘制直方图和QQ图 都是用来看数据分布是否是正太分布的
-def paint_dist(data, columns, cols=6):
-    rows = len(columns)
-    plt.figure(figsize=(60, 20), dpi=50)
-
-    i = 0
-    for col in columns:
-        print(data[col])
-        # if np.all(data[col]) == 0:
-        #     continue
-        i += 1
-        ax = plt.subplot(7, 6, i)
-        sns.distplot(data[col], fit=stats.norm)
-        i += 1
-        ax = plt.subplot(7, 6, i)
-        res = stats.probplot(data[col], plot=plt)
-
-    plt.tight_layout()
-    plt.show()
+"""
+训练数据和测试数据关系
+"""
 
 
 def paint_kde(train_data, test_data, column: str, y):
@@ -125,60 +218,3 @@ def paint_kde(train_data, test_data, column: str, y):
     ax.set_xlabel(column)
     ax.set_ylabel(y)
     ax = ax.legend('train', 'test')
-
-
-def paint_reg(train_data, column: str, y_name='target'):
-    """
-    绘制线性回归关系
-    :param y_name:
-    :param train_data:
-    :param column: 训练数据所在的列名
-    :return:
-    """
-    fools = 2
-    frows = 1
-    plt.figure(figsize=(8, 4), dpi=50)
-    ax = plt.subplot(1, 2, 1)
-    sns.regplot(x=column, y=y_name, data=train_data, ax=ax, scatter_kws={'marker': '.', 's': 3, 'alpha': 0.3},
-                line_kws={'color': 'k'})
-    plt.xlabel(column)
-    plt.ylabel(y_name)
-
-    ax = plt.subplot(1, 2, 2)
-    sns.displot(train_data[column].dropna())
-    plt.xlabel(column)
-
-    plt.show()
-
-
-def paint_heatmap(data: DataFrame, drop_columns,k):
-    """
-    绘制热力图
-    :type k: 阈值k
-    :return:
-    """
-    pd.set_option("display.max_columns", 10)
-    pd.set_option('display.max_rows', 10)
-    if drop_columns is not None:
-        data_1 = data.drop(drop_columns, axis=1)
-    else:
-        data_1 = data
-
-    data_corr = data_1.corr()
-    # 这个是整体的热力图
-    # ax = plt.subplots(figsize=(20, 16))
-    # ax = sns.heatmap(data_corr, vmax=.8, square=True, annot=True)
-    # plt.show()
-
-    # 这个是top10的热力图
-    cols = data_corr.nlargest(k, 'target')['target'].index
-    cm = np.corrcoef(data_1[cols].values.T)
-    hm = plt.subplots(figsize=(10, 10))
-    hm = sns.heatmap(data_1[cols].corr(), annot=True, square=True)
-    plt.show()
-
-
-def paint_violin(data: DataFrame, column):
-    plt.figure(figsize=[16, 10])
-    sns.violinplot(x=data['label'], y=data[column])
-    plt.show()
